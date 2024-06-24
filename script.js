0b4676980b49f93e03c62c1b08908529f1c5986e
Deepanshu112 <140751260+Deepanshu112@users.noreply.github.com>
2024-06-24 23:08:34 +0530

    Add files via upload
    Dynamic file

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ let toggle = false;
             // Toggle the state
             toggle = !toggle;
         });
+
+
+
+
+$(document).ready(function(){
+    $.getJSON("coupon.json",function(data){
+        console.log(data);
+
+
+    $('.parent .part2 h1').html(data.tasks[0].task_title);
+    $('.parent .part2 p').html(data.tasks[0].task_description);
+    $('.parent .part1 p').html(data.title);
+
+
+    $('.parent .grid #card1 .head h1').html(data.tasks[0].assets[0].asset_title);
+    $('.parent .grid #card2 .head h1').html(data.tasks[0].assets[1].asset_title);
+    $('.parent .grid #card3 .head h1').html(data.tasks[0].assets[2].asset_title);
+    $('.parent .grid #card4 .head h1').html(data.tasks[0].assets[3].asset_title);
+
+    $('.parent .grid #card1 p').html(data.tasks[0].assets[0].asset_description);
+    $('.parent .grid #card2 p').html(data.tasks[0].assets[1].asset_description);
+    $('.parent .grid #card3 p').html(data.tasks[0].assets[2].asset_description);
+    $('.parent .grid #card4 p').html(data.tasks[0].assets[3].asset_description);
+    
+    })
+    .fail(function(){
+        console.log("Check your code");
+    })
+})
